19211dcccd3a44b26efc4061c409af8b4bc4a50c
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-01-14 02:34:36 +0530

    see

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,6 @@
 import Home from './Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
 export default function App() {
   return (
     <Router>
--- Frontend/src/Home/Home.jsx
+++ Frontend/src/Home/Home.jsx
@@ -4,9 +4,7 @@ import Hero from "../components/Hero/hero";
 import AboutSection from "../components/About/about";
 import Faqs from "../components/Faq/faq";
 import Footer from "../components/Footer/footer";
-import Challenges from "../components/CaseInfo/Challenges";
 import TeamMembers from "../components/Team_Members/team";
-
 import Police_team from "../components/Police_Team/Police_team";
 import Details from "../components/Details/Details";
 import Carousel from "../components/carousel/carousel";
@@ -42,22 +40,15 @@ function Home() {
       bg-no-repeat bg-center"
         >
           <Navbar />
-
           <div className="relative z-0">
             <Hero />
           </div>
-
           <AboutSection />
           <Carousel />
           <Statement/>
-          {/* <Carousel /> */}
-          {/* <Fir /> */}
           <Details />
-          {/* <Procedure /> */}
           <Carousel />
-          {/* <Challenges /> */}
           <Police_team />
-
           <TeamMembers />
           <Faqs />
           <Footer />
