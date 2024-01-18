118ae93fb4e8460dd9830405ee0dd29b8eb9fb23
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-01-18 11:05:40 +0530

    font

--- Frontend/src/components/Details/Gallary.jsx
+++ Frontend/src/components/Details/Gallary.jsx
@@ -5,6 +5,7 @@ import { thumb} from '../../assets/images';
 import Design_copmponent from '../design_componet/opacity';
 import axios from 'axios';
 import { useState,useEffect } from 'react';
+// import {Fir} from './content.json'
 
 const Gallery = ({ activeTab }) => {
   
@@ -12,7 +13,7 @@ const Gallery = ({ activeTab }) => {
   useEffect(()=>{
     async function Details(){
       try{
-        const showDetails = await axios.get(' ');
+        const showDetails = await axios.get("http://127.0.0.1:8000/api/ocr/file/");
         console.log(showDetails.data);
         SetDetails(showDetails.data);
 
@@ -23,7 +24,8 @@ const Gallery = ({ activeTab }) => {
     Details();
   },[])
 
-  const filteredData = tabData[activeTab] || [];
+  const filteredData = tabData;
+  // const filteredData = Fir || [];
 
   return (
     <div className="container w-full h-full mt-12 mb-12">
@@ -38,34 +40,50 @@ const Gallery = ({ activeTab }) => {
               style={{ maxWidth: '33%', maxHeight: '33%' }}
             />
             <div className='flex flex-col gap-8 text-white'>
-            <h1 className='text-xl md:text-2xl lg:text-4xl font-bold '>{data.heading}</h1>
-            <p className="text-white">{data.content}</p>
+            <h1 className='text-xl md:text-2xl lg:text-4xl font-bold '>Details</h1>
+            <p className="text-white">{data.Fir.FIR.DetailsOfAccused}</p>
+            <p className="text-white">Description:  {data.Fir.FIR.Description}</p>
+            <div className='text-xl text-bold '>Sections:-</div>
+            
+            <p className="text-white">Act: {data.Fir.FIR.Act}</p>
+            <p className="text-white">Cognizable: {data.Fir.FIR.Cognizable}</p>
+            <p className="text-white">Bailable:  {data.Fir.FIR.Bailable}</p>
             </div>
           </div>
           {/* 2nd div - Image */}
           <div className='flex justify-center items-center'>
-            <img
+          <p className="text-white">Occurrence Of Offence:</p><br/>
+          <div className='container flex'>
+          <p className="text-white m-2">
+           Date:{data.Fir.OccurrenceOfOffence.Date},
+          </p>
+          <p className="text-white m-2">Day:{data.Fir.OccurrenceOfOffence.Day},</p>
+          <p className="text-white m-2">Time:{data.Fir.OccurrenceOfOffence.Time}</p>
+          </div>
+            {/* <img
               src={data.img1}
               alt={`Image 1`}
               className=" object-contain"
               style={{ maxWidth: '100%', maxHeight: '100%' }}
-            />
+            /> */}
+            
           </div>
           {/* 3rd div - Image */}
           <div className='flex justify-center items-center '>
-            <img
+          {/* <p className="text-white">{data.Fir.FIR['FIR No.']}</p> */}
+            {/* <img
               src={data.img2}
               alt={`Image 2`}
               className=" object-contain"
               style={{ maxWidth: '100%', maxHeight: '100%' }}
-            />
+            /> */}
           </div>
           {/* 4th div - Text */}
           <div className="py-4 mt-4 text-white">
           <Design_copmponent/>
           {/* <h1 className='font-bold pb-8  '>More Info</h1> */}
-            <p className="text-white mt-3">{data.Cognizable}</p>
-            <p className="text-white">{data.Bailable}</p>
+            {/* <p className="text-white mt-3">{data.Cognizable}</p> */}
+            {/* <p className="text-white">{data.Bailable}</p> */}
           </div>
         </div>
       ))}
--- Frontend/src/components/Details/Tabs_Data.jsx
+++ Frontend/src/components/Details/Tabs_Data.jsx
@@ -7,35 +7,105 @@ import { details1,
   section1,
   section2, } from "../../assets/images";
 
-export const  tabData = [
-    [{
-        heading: "General Details",
-        content: "Lorem Loremrrupti ullam voluptas consequatur nihil nobis, incidunt vel adipisci optio laborum repudiandae atque eligendi commodi similique laudantium et numquam iste? Placeat, quasi?",
-        img1: details1,
-        img2: details2,
-        Cognizable: "Non Cognizable",
-        Bailable: "Bailable",
-      }],
-      [{
-        heading: "Suggested Sections and acts available",
-        content: "Corrupt nihil nobis, incidunt vel adipisci optio laborum repudiandae atque eligendi commodi similique laudantium et numquam iste? Placeat, quasi?  ",
-        img1: section1,
-        img2: section2,
-        prize_amt: "50,000",
-      }],
-      [{
-        heading: "Punishments",
-        content: "Corrupti ullam voluptas consequatur nihil nobis, incidunt  repudiandae atque eligendi commodi similique laudantium et numquam iste? Placeat, quasi?   ",
-        img1: punishment1,
-        img2: punishment2,
-        prize_amt: "30,000",
-      }],
-      [{
-        heading: "Court Details",
-        content: "Corrupti ullam voluptas consequatur nihil nobis, incidunt vel adipisci optio laborum  commodi similique laudantium et numquam iste? Placeat, quasi? ",
-        img1: court2,
-        img2: court1,
-        prize_amt: "3,000",
-      }],
+export const  tabData =[
+  {
+    "Fir": {
+        "FIR": {
+          "District": "",
+          "P.S": "",
+          "Year": "",
+          "FIR No.": "",
+          "Date": "",
+          
+              "Act": "Indian Penal Code",
+              
+                  "SectionNo": "154",
+                  "Cognizable": "true",
+                  "Description": "Information in cognizable cases.",
+                  "Bailable": "Yes"
+                
+              
+            },
+          
+          "OccurrenceOfOffence": {
+            "Day": "21 Dec",
+            "Date": "Thursday",
+            "Time": " "
+          },
+          "InformationReceivedAtPS": {
+            "Date": "",
+            "Time": ""
+          },
+          "GeneralDiaryReference": {
+            "EntryNo": "",
+            "Time": ""
+          },
+          "TypeOfInformation": "",
+          "PlaceOfOccurrence": {
+            "DirectionAndDistanceFromPS": "",
+            "BeatNo": "",
+            "Address": "",
+            "NameOfPSIfOutsideLimit": "",
+            "District": ""
+          },
+          "ComplainantInformation": {
+            "Name": "",
+            "FatherOrHusbandName": "",
+            "DateOfBirth": "",
+            "Nationality": "",
+            "PassportNo": "",
+            "DateOfIssue": "",
+            "PlaceOfIssue": "",
+            "Occupation": "",
+            "Address": ""
+          },
+          "DetailsOfAccused": "ajie",
+          "ReasonsForDelayInReporting": "",
+          "ParticularsOfPropertiesStolenOrInvolved": "",
+          "TotalValueOfProperties": "",
+          "InquestReportUDCaseNo": "",
+          "FIRContents": "",
+          "ActionTaken": "",
+          "OfficerInCharge": {
+            "Name": "",
+            "Rank": "",
+            "No": ""
+          },
+          "SignatureOrThumbImpression": "",
+          "DateAndTimeOfDespatchToTheCourt": ""
+        }
+      }
+    
+]
+//  [
+//     [{
+//         heading: "General Details",
+//         content: "Lorem Loremrrupti ullam voluptas consequatur nihil nobis, incidunt vel adipisci optio laborum repudiandae atque eligendi commodi similique laudantium et numquam iste? Placeat, quasi?",
+//         img1: details1,
+//         img2: details2,
+//         Cognizable: "Non Cognizable",
+//         Bailable: "Bailable",
+//       }],
+//       [{
+//         heading: "Suggested Sections and acts available",
+//         content: "Corrupt nihil nobis, incidunt vel adipisci optio laborum repudiandae atque eligendi commodi similique laudantium et numquam iste? Placeat, quasi?  ",
+//         img1: section1,
+//         img2: section2,
+//         prize_amt: "50,000",
+//       }],
+//       [{
+//         heading: "Punishments",
+//         content: "Corrupti ullam voluptas consequatur nihil nobis, incidunt  repudiandae atque eligendi commodi similique laudantium et numquam iste? Placeat, quasi?   ",
+//         img1: punishment1,
+//         img2: punishment2,
+//         prize_amt: "30,000",
+//       }],
+//       [{
+//         heading: "Court Details",
+//         content: "Corrupti ullam voluptas consequatur nihil nobis, incidunt vel adipisci optio laborum  commodi similique laudantium et numquam iste? Placeat, quasi? ",
+//         img1: court2,
+//         img2: court1,
+//         prize_amt: "3,000",
+//       }],
       
-  ];
+//   ];
